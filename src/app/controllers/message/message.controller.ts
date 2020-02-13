a32a1b481ed1fff3e281af4e525f1ab73b7eed38
Raphael Ndauwa (Ubuntu) <raphaelndauwa@gmail.com>
2020-02-13 11:52:57 +0300

    add messages faker

--- src/app/controllers/message/message.controller.ts
+++ src/app/controllers/message/message.controller.ts
@@ -1,7 +1,7 @@
 import IControllerBase from "../interfaces/IControllerBase.interface";
 import express, {Request, Response} from "express";
-import Message from "../../models/message.model";
 import MessageControllerHelper from "./resource/message.controller.helper";
+import faker from 'faker';
 
 class MessageController implements IControllerBase {
     public path = '/';
@@ -12,8 +12,9 @@ class MessageController implements IControllerBase {
     }
 
     public initRoutes() {
-        this.router.get('/messages', this.index)
-        this.router.post('/messages', this.post)
+        this.router.get('/messages', this.index);
+        this.router.post('/messages', this.post);
+        this.router.post('/messages/fake', this.fake);
     }
 
     index = (req: Request, res: Response) => {
@@ -22,11 +23,21 @@ class MessageController implements IControllerBase {
 
     post = async (req: Request, res: Response) => {
         try {
-           let message = await MessageControllerHelper.saveMessage(req.body);
-           console.log(message);
-           
+            let message = await MessageControllerHelper.saveMessage(req.body);
+            console.log(message);
+
         } catch (e) {
             console.log(e, 'error saving message')
+        }
+    };
+
+    fake = async (req: Request, res: Response) => {
+        const {count} = req.body;
+        for(var counter:number = 1; counter < parseInt(count); counter++){
+            await MessageControllerHelper.saveMessage({
+                title: faker.name.findName(),
+                message: faker.lorem.paragraph()
+            })
         }
     };
 
